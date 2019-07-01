7abc79c9f125be467046e8e7818e90d52baf1986
Mateusz Niemczyk <mateusz.niemczyk@cloudrise.pl>
2019-07-01 20:42:01 +0200

    added func to format mac addresses

--- main.py
+++ main.py
@@ -1,16 +1,38 @@
 import mysql.connector
+import argparse
 import private.config ## edit here to config
 
 
-def generate_user_list():
+def get_all_users():
 
     connection = mysql.connector.connect(**private.config.DATABASE_CONFIG)
     cursor = connection.cursor()
-    query = ("SELECT first_name, last_name, hex(mac), inet_ntoa(ip) FROM clients")
+    query = ("SELECT id, first_name, last_name, hex(mac), inet_ntoa(ip) FROM clients")
     cursor.execute(query)
 
-    for (first_name, last_name, mac, ip) in cursor:
-        print(first_name, last_name, mac, ip)
+    user_list = cursor.fetchall()
+
+    connection.close()
+
+    return user_list
+
+def format_mac_address(mac):
+    return ':'.join(mac[i:i+2] for i in range(0, len(mac), 2))
+
+def print_user_details():
+    user_list = get_all_users()
+    for user in user_list:
+        print(user[0], user[1], user[2], format_mac_address(user[3]), user[4])
 
 
-generate_user_list()
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("action", help="Select one of the following action: list")
+    args = parser.parse_args()
+
+    if args.action == "list":
+        print_user_details()
+
+if __name__ == '__main__':
+    main()
