ec5cd43be924ff72c342852cb622f66b8b9e3f19
Mateusz Niemczyk <mateusz.niemczyk@cloudrise.pl>
2019-07-15 15:24:08 +0200

    removed config file from root dir, added comments to iptables section

--- scripts/iptables-config.sh
+++ scripts/iptables-config.sh
@@ -1,6 +1,7 @@
+### SSH RULE ###
 iptables -A INPUT -p tcp -s YOUR_IP --dport 22 -j ACCEPT
 iptables -A INPUT -p tcp -s 0.0.0.0/0 --dport 22 -j DROP
 
-
+### MYSQL RULE ###
 iptables -A INPUT -p tcp -s YOUR_IP --dport 3306 -j ACCEPT
 iptables -A INPUT -p tcp -s 0.0.0.0/0 --dport 3306 -j DROP
